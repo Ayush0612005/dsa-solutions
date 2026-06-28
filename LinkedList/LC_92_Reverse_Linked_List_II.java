c4b4f81bc16057c8a53463494eb1d2221565717f
Ayush0612005 <ayushkulshreshtha37@gmail.com>
2026-06-28 17:26:18 +0530

    LC_92_Reverse_Linked_List_II.java

---
+++ LinkedList/LC_92_Reverse_Linked_List_II.java
@@ -0,0 +1,64 @@
+/*
+LeetCode 92 - Reverse Linked List II
+
+Difficulty: Medium
+
+Approach:
+- Use a dummy node to simplify reversing when the head is involved.
+- Move a pointer to the node just before the left position.
+- Reverse the sublist by repeatedly moving the next node of the
+  sublist to the front of the reversed portion.
+- Reconnect the reversed sublist with the remaining list.
+- Return the new head of the list.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) {
+ *         this.val = val;
+ *     }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        if (head == null || left == right) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode start = prev.next;
+        ListNode then = start.next;
+
+        for (int i = 0; i < right - left; i++) {
+
+            start.next = then.next;
+            then.next = prev.next;
+            prev.next = then;
+            then = start.next;
+        }
+
+        return dummy.next;
+    }
+}
