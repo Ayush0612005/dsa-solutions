29d4df86c08127847f9cb9bfab6ee9dec31e6b7a
Ayush Kulshreshtha <ayushkulshreshtha37@gmail.com>
2026-09-13 19:59:07 +0530

    Update LC_121_Best_Time_to_Buy_and_Sell_Stock.java

--- Arrays/LC_121_Best_Time_to_Buy_and_Sell_Stock.java
+++ Arrays/LC_121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -34,3 +34,27 @@ class Solution {
         return maxProfit;
     }
 }
+
+
+
+// Another way of solving the same question:
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+
+        for(int i = 1; i < prices.length; i++){
+
+            int profit = prices[i] - min;
+
+            max = Math.max(max, profit);
+            min = Math.min(min,prices[i]);
+
+        }
+        return max;
+    }
+}
+
+
+//same method as above 2 but writing style is different:
