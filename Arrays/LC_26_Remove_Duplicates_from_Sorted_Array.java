1f1eee152824e1c2d66ed541301f042d4a2660e5
Ayush Kulshreshtha <ayushkulshreshtha37@gmail.com>
2026-09-13 20:51:58 +0530

    Update LC_26_Remove_Duplicates_from_Sorted_Array.java

--- Arrays/LC_26_Remove_Duplicates_from_Sorted_Array.java
+++ Arrays/LC_26_Remove_Duplicates_from_Sorted_Array.java
@@ -38,3 +38,19 @@ class Solution {
         return i + 1;
     }
 }
+
+
+//best method is given below:
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
