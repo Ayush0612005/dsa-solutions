527f66f8c2fe77d3356a4b07db5d5b93dc0e3f8c
Ayush Kulshreshtha <ayushkulshreshtha37@gmail.com>
2026-09-13 20:54:07 +0530

    Update LC_27_Remove_Element.java

--- Arrays/LC_27_Remove_Element.java
+++ Arrays/LC_27_Remove_Element.java
@@ -19,3 +19,18 @@ class Solution {
         return index;
     }
 }
+
+
+//easy 
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
