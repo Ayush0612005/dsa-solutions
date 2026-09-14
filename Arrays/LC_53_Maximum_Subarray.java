5004a6e80fe20ae3ddb62990037e95a00cf3053e
Ayush Kulshreshtha <ayushkulshreshtha37@gmail.com>
2026-09-14 15:42:14 +0530

    Update LC_53_Maximum_Subarray.java

--- Arrays/LC_53_Maximum_Subarray.java
+++ Arrays/LC_53_Maximum_Subarray.java
@@ -33,3 +33,25 @@ class Solution {
         return maxSum;
     }
 }
+
+
+//another way os solving the same question
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentsum = nums[0];
+        int maxsum = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(currentsum + nums[i] < nums[i]){
+                currentsum = nums[i];
+            }else{
+                currentsum += nums[i];
+            }
+            if(currentsum > maxsum){
+                maxsum = currentsum;
+            }
+        }
+        return maxsum;
+    }
+}
