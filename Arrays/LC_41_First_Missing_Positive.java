18e1b8321092f7037427fb31dd99fd1e0e16e1fe
Ayush Kulshreshtha <ayushkulshreshtha37@gmail.com>
2026-09-21 23:57:00 +0530

    Update LC_41_First_Missing_Positive.java

--- Arrays/LC_41_First_Missing_Positive.java
+++ Arrays/LC_41_First_Missing_Positive.java
@@ -31,3 +31,33 @@ class Solution {
         return expected;
     }
 }
+
+
+//O(n) time complexity and O(1)  space complexity code is given below:
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            while (nums[i] >= 1 && nums[i] <= n
+                    && nums[nums[i] - 1] != nums[i]) {
+
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
