c3f1fa6728f161e9051708136b7b41e9b633d32c
Ayush Kulshreshtha <ayushkulshreshtha37@gmail.com>
2026-09-22 11:02:32 +0530

    Update LC_20_Valid_Parentheses.java

--- Stacks&Queues/LC_20_Valid_Parentheses.java
+++ Stacks&Queues/LC_20_Valid_Parentheses.java
@@ -53,3 +53,26 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+//another easy way of doing the same question using Deque
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char ch : s.toCharArray()){
+        
+        if(ch == '('){
+            stack.push(')');
+        }else if(ch == '{'){
+            stack.push('}');
+        }else if(ch == '['){
+            stack.push(']');
+        }else if(stack.isEmpty() || stack.pop() != ch){
+            return false;
+        }
+           
+        }
+        return stack.isEmpty();
+    }
+
+}
